7fd1206fbce89450a06e2840107ae148c8b57008
Osman Ungur <osmanungur@gmail.com>
2018-02-12 21:55:36 +0100

    Added example with sample data

--- index.php
+++ index.php
@@ -3,9 +3,12 @@
 
 require_once 'vendor/autoload.php';
 
-
 $plat = new \Osman\MarsRoverChallenge\Model\Plateau(new \Osman\MarsRoverChallenge\Model\Coordinate(5,5));
+
+$rover = new \Osman\MarsRoverChallenge\Rover(new \Osman\MarsRoverChallenge\Model\Coordinate(1,2), new \Osman\MarsRoverChallenge\Model\Orientation\North(), $plat);
+$rover->run('LMLMLMLMM');
+echo $rover->getCurrentPosition() . PHP_EOL;
 
 $rover = new \Osman\MarsRoverChallenge\Rover(new \Osman\MarsRoverChallenge\Model\Coordinate(3,3), new \Osman\MarsRoverChallenge\Model\Orientation\East(), $plat);
 $rover->run('MMRMMRMRRM');
-echo $rover->getCurrentPosition();
+echo $rover->getCurrentPosition() . PHP_EOL;
